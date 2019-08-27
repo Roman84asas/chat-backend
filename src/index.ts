f6ac38761f8a28dd0b9a743a2f6cec6f1be70457
RomanMereneanu <asasin84@mail.ru>
2019-08-28 00:37:54 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import bodyParser from "body-parser";
 
 import User from "./models/User";
 
 const app = express();
 
+app.use(bodyParser.json());
+
 mongoose.connect('mongodb://localhost:27017/mychat', {useNewUrlParser: true});
 
-const user = new User({ email: "hello@mail.ru", fullname: "Test schema" });  
-  user.save().then(() => console.log('meow'));
+//const user = new User({ email: "hello@mail.ru", fullname: "Test schema" });  
+//user.save().then(() => console.log('meow'));
 
-app.get('/', function ( _: any, res: any) { 
-  res.send('Hello World!');
+app.post('/create', function ( req: any, res: any) { 
+  console.log(req.body);
+  res.send();
   
 });
 
-app.listen(3030, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
