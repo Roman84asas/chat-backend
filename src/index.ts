37dc6ea066e88991c592ba677247a7502d0bc260
Roman84asas <roma.mereneanu@mail.ru>
2019-08-28 19:42:01 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import bodyParser from "body-parser";
 
 import User from "./models/User";
 
 const app = express();
 
+app.use(bodyParser.json());
+
 mongoose.connect('mongodb://localhost:27017/mychat', {useNewUrlParser: true});
 
-const user = new User({ email: "hello@mail.ru", fullname: "Test schema" });  
-  user.save().then(() => console.log('meow'));
+app.post('/create', function ( req: any, res: any) { 
+ const postData = {
+   email: req.body.email,
+   fullname: req.body.fullname,
+   password: req.body.password
+ }  
+  const user = new User(postData);
 
-app.get('/', function ( _: any, res: any) { 
-  res.send('Hello World!');
-  
+  user
+  .save()
+  .then((obj: any) => {
+    res.json(obj);
+  }) 
+  .catch(reason => {
+      res.json(reason);
+  });
 });
 
 app.listen(3030, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3030!');
 });
